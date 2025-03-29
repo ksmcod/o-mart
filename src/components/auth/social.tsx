c4f83bdf220d99555d8aab69170fdf0661ab59bc
Kono Steve <stevekono77@gmail.com>
2025-03-29 04:30:45 +0100

    feat: created google sign in button in 'Social.tsx' component

--- src/components/auth/social.tsx
+++ src/components/auth/social.tsx
@@ -4,10 +4,12 @@ import Image from "next/image";
 import { signIn } from "next-auth/react";
 import { Button } from "@/components/ui/button";
 import GithubIcon from "@/logos/github/github-mark-white.png";
+import GoogleIcon from "@/logos/google/google.png";
 
 export default function Social() {
   return (
-    <div>
+    <div className="space-y-3">
+      {/* Github Sign in button */}
       <Button
         onClick={() => signIn("github")}
         className="w-full h-auto py-2 flex items-center gap-2"
@@ -18,6 +20,19 @@ export default function Social() {
           className="size-7 text-white"
         />
         <span className="font-bold text-base">Continue with GitHub</span>
+      </Button>
+
+      {/* Google Sign in button */}
+      <Button
+        onClick={() => signIn("google")}
+        className="w-full h-auto py-2 flex items-center gap-2"
+      >
+        <Image
+          src={GoogleIcon}
+          alt="Google logo"
+          className="size-7 text-white"
+        />
+        <span className="font-bold text-base">Continue with Google</span>
       </Button>
     </div>
   );
